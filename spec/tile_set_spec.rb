ea14b990e483ea0caca7bf18da53812621f82d84
Peter Storum <p-storum@hotmail.com>
2022-03-17 19:42:40 -0700

    testing for generating tiles of specific size

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -7,11 +7,11 @@ describe 'A tile set' do
     it 'returns an array of 9 tiles by default' do
       tile_set = TileSet.generate
       expect((tile_set).count).to eq 9
-      ##raise "Start here"
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      tile_set = TileSet.generate(5)
+      expect((tile_set).count).to eq 5
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
